ebe69cb894b129171b0e0370e5906155f61d4dc0
alanmulero <alan.mulero@gmail.com>
2026-02-19 17:03:47 -0300

    readonly

--- public/modelo/Filmes.php
+++ public/modelo/Filmes.php
@@ -1,21 +1,57 @@
 <?php
 
-class Filmes{
+class Filmes
+{
+
+    public array $somaNotas;
+
+    public function __construct(
+        public readonly string $nome,
+        public readonly int    $anoLancamento,
+        public readonly string $genero,
+        private float          $nota = 0.0
+    )
+    {
+        $this->somaNotas = [$nota];
+    }
+
+    public function totalNotas(): float
+    {
+
+        $resultadoSoma = array_sum($this->somaNotas);
+        return $resultadoSoma;
+    }
 
 
-	
-
-	public function __construct(
-
-	public string $nome,
-	public int $anoLancamento,
-	public string $genero,
-	public float $nota
-
-	){}
+    public function media(): float
+    {
 
 
+        if (count($this->somaNotas) === 0) {
+            return 0.0;
+        } else {
+            return $this->totalNotas() / 2.0;
+        }
 
+
+    }
+
+
+    public function setSomaNotas(array $somaNotas): void
+    {
+        $this->somaNotas = $somaNotas;
+    }
+
+
+    public function getNota(): float
+    {
+        return $this->nota;
+    }
+
+    public function setNota(float $nota): void
+    {
+        $this->nota = $nota;
+    }
 
 
 }
--- public/modelo/index.php
+++ public/modelo/index.php
@@ -6,17 +6,32 @@ require_once __DIR__ . '/../../src/funcoes.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filme Registrado</title>
 </head>
+
 <body>
     <h1>Filme Registrado com Sucesso!</h1>
     <?php
-        $novoFilme = new Filmes($_POST['nome'] ?? 'Não Informado', $_POST['anoLancamento'] ?? 0, $_POST['genero'] ?? '', $_POST['nota'] ?? 0.0);
-        imprimeFilme($novoFilme);
+    $novoFilme = new Filmes($_POST['nome'] ?? 'Não Informado', $_POST['anoLancamento'] ?? 0, $_POST['genero'] ?? '', $_POST['nota'] ?? 0.0);
+    imprimeFilme($novoFilme);
+    ?>
+
+    <br>
+    <h4>Apresentando resultado das funções:</h4>
+    <?php
+
+    echo $novoFilme->totalNotas();
+    echo "<br>";
+    echo $novoFilme->media();
+
+
+
     ?>
     <br><a href="../index.html">Voltar ao formulário</a>
 </body>
+
 </html>
